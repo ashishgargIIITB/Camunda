1d15a07e6614a1bb8a2732743d933b998021e17e
Ashish Garg <ashish.garg@iiitb.net>
2020-10-08 23:22:37 +0530

    Commiting initial code to Git

---
+++ trainingCode/src/main/java/org/camunda/bpm/training/trainingCode/DeductExistingCredit.java
@@ -0,0 +1,23 @@
+package org.camunda.bpm.training.trainingCode;
+
+
+
+
+import java.util.Random;
+import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.camunda.bpm.engine.delegate.JavaDelegate;
+
+public class DeductExistingCredit implements JavaDelegate
+{
+    public void execute(final DelegateExecution execution) throws Exception {
+        final Random rando1 = new Random();
+        final int payableAmount = (int)execution.getVariable("paymentAmount");
+        execution.setVariable("name", (Object)"ashish");
+        execution.setVariable("remainingAmount", (Object)(payableAmount - 100));
+        execution.setVariable("generateFailure", (Object)rando1.nextBoolean());
+        System.out.println("********************************************************");
+        System.out.println("Check Weather java file executed");
+        final LoggerDelegate logger = new LoggerDelegate();
+        logger.log("Inside CheckWeather Class for deducting from existing Credit");
+    }
+}
