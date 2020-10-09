0bc57666d96e71006e5429e65b87de06a2215bc6
Ashish Garg <ashish.garg@iiitb.net>
2020-10-09 19:37:50 +0530

    Completed Exercise 7 POM file updated as SLF4j error was coming

--- TaskCompletion/src/main/java/org/camunda/bpm/TaskCompletion/ExternalTaskCompletion.java
+++ TaskCompletion/src/main/java/org/camunda/bpm/TaskCompletion/ExternalTaskCompletion.java
@@ -2,13 +2,13 @@ package org.camunda.bpm.TaskCompletion;
 
 
 import org.camunda.bpm.client.ExternalTaskClient;
-import org.camunda.bpm.engine.variable.Variables;
-import org.camunda.bpm.engine.variable.value.ObjectValue;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
+//import org.camunda.bpm.engine.variable.Variables;
+//import org.camunda.bpm.engine.variable.value.ObjectValue;
+//
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.Collections;
+//import java.util.List;
 
 
 public class ExternalTaskCompletion  {
@@ -22,7 +22,9 @@ public class ExternalTaskCompletion  {
 			    client.subscribe("checkingExternalTask_Topic")
 			      .lockDuration(1000)
 			      .handler((externalTask, externalTaskService) -> {
-
+			    	
+			    	  boolean isTaskFailed = externalTask.getVariable("isTaskFailed");
+				        if(!isTaskFailed) {
 			        // retrieve a variable from the Workflow Engine
 			        int paymentAmount = externalTask.getVariable("paymentAmount");
 
@@ -38,7 +40,11 @@ public class ExternalTaskCompletion  {
 //			          Collections.singletonMap("creditScores", creditScoresObject));
 			        System.out.println("Payment Amount here was: "+paymentAmount);
 			        System.out.println("The External Task " + externalTask.getId() + " has been completed!");
-
+			      }
+			      else {
+		        		externalTaskService.handleBpmnError(externalTask, "externalSystemIssueErrorCode");//, "raisingErrorMessage");
+		        		 System.out.println("Error has been rasied for " + externalTask.getId());
+		        }
 			      }).open();
 
 	}
